78a4de6cca9de37045cf9cecf8aa42171c0b8f73
K <kemilbeltre@gmail.com>
2021-08-01 12:10:53 +0100

    fix directory name

--- descending-order/solutions/descending-order.ts
+++ descending-order/solutions/descending-order.ts
@@ -1,8 +1,8 @@
 export function descendingOrder(numbers: number) {
   if (Math.sign(numbers) === 1) {
-     let arrNumbers = numbers.toString().split('').sort (
-       (a:any, b:any) => b - a ).join('');
-     return parseInt(arrNumbers);
+    let arrNumbers = numbers.toString().split('').sort (
+      (a:any, b:any) => b - a ).join('');
+    return parseInt(arrNumbers);
   }
   else if (numbers === 0){
     return  0
